cfb659d7d651cc98bc34af3eb1520d35cb9206db
Puibusque <baptiste.puibusque@limayrac.fr>
2021-03-28 20:53:05 +0200

    fin du tp bis

--- TU_CDate.cpp
+++ TU_CDate.cpp
@@ -1,10 +1,10 @@
 /*************************************************/
 // Nom du projet: Surcharge d'operateur
-// Nom du fichier: tp surcharge d'operateur
+// Nom du fichier: TU_CDate
 // Version : 1.0
 // Nom du programmeur: Baptiste Puibusque
 // Date de création :22/03/2021
-// Rôle du fichier: main (pour test essentiellement
+// Rôle du fichier: mTest utnitaire de la classe CDate
 // Nom des composants utilises: iostream, CDate.h
 // Historique du fichier:
 /*************************************************/
@@ -12,11 +12,14 @@
 #include <iostream>
 #include "CDate.h"
 
+#include <stdio.h>      /* printf */
+#include <time.h>       /* time_t, time, ctime */
+
 using namespace std;
 
 int main()
 {
-   /* CDate date1(12,04,2021,18,45,12);
+    CDate date1(12,04,2021,18,45,12);
     CDate date2(28,02,2021,22,45,12);
 
     if(date1==date2){
@@ -44,13 +47,6 @@ int main()
 
     cout<<"date 1 :"<<jour<<'/'<<mois<<'/'<<annee<<' '<<heure<<':'<<minute<<':'<<seconde<<endl;
 
-    while(true){
-        date2.operator++();
 
-        date2.getDate(jour,mois,annee,heure,minute,seconde);
-    cout<<"date 2 :"<<jour<<'/'<<mois<<'/'<<annee<<' '<<heure<<':'<<minute<<':'<<seconde<<endl;
-
-    }*/
-    return 0;
 
 }
